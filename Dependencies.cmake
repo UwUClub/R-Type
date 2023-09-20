6852f37aac02ce419e6c06eead9ff9e727b838c0
Mathéo Martin <matheo.martin@epitech.eu>
2023-09-20 10:20:44 +0200

    Remove msvc compilation in cicd

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -7,6 +7,7 @@ function(R_Type_setup_dependencies)
 
   find_package(Catch2)
   find_package(Boost)
+  find_package(SDL2)
 
   if(NOT TARGET Catch2::Catch2WithMain)
     cpmaddpackage("gh:catchorg/Catch2@3.3.2")
@@ -21,5 +22,13 @@ function(R_Type_setup_dependencies)
     )
   endif()
 
+  if (NOT TARGET SDL2::SDL2)
+    CPMAddPackage(
+            NAME sdl2
+            VERSION 2.28.3
+            GITHUB_REPOSITORY "libsdl-org/SDL"
+            GIT_TAG "release-2.28.3"
+    )
+  endif()
 
 endfunction()
