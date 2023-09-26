f402e209ede965ca396af306d187d59f160557d7
Glassalo <maxence.labourel@epitech.eu>
2023-09-26 10:11:31 +0200

    Upgraded ResourcesManager to unload resources

--- src/ecs/Resources/ResourcesManager.hpp
+++ src/ecs/Resources/ResourcesManager.hpp
@@ -8,6 +8,7 @@
 #ifndef RESSOURCESMANAGER_HPP_
 #define RESSOURCESMANAGER_HPP_
 
+#include <algorithm>
 #include <any>
 #include <functional>
 #include <memory>
@@ -38,54 +39,98 @@ namespace ECS::Core {
             ResourcesManager &operator=(ResourcesManager &&aOther) noexcept = default;
 
             //-------------- PUBLIC METHODS --------------//
+
             /**
-             * @brief Load a resource in the manager
+             * @brief Load the resource if it's not already loaded
              *
-             * @tparam Resource The type of the resource to load
-             * @param aPath The path of the resource
-             * @param aLoader The function that load the resource
-             * @return Resource& The resource
+             * @tparam Resource The type of the resource
+             * @param aPath The path to the resource
+             * @return Resource& A reference to the resource
              */
             template<class Resource>
-            Resource &loadRessource(std::string &aPath, std::function<Resource &(std::string &path)> aLoader)
+            Resource &load(const std::string &aPath)
             {
-                if (_resources.find(std::type_index(typeid(Resource))) == _resources.cend()) {
-                    _resources[std::type_index(typeid(Resource))] = boost::container::flat_map<std::string, std::any>();
+                try {
+                    auto isLoaded = _resources.find(aPath);
+                    if (isLoaded != _resources.end()) {
+                        return std::any_cast<Resource &>(isLoaded->second);
+                    }
+
+                    auto &loader = _loaders[std::type_index(typeid(Resource))];
+                    auto &resource = loader(aPath);
+
+                    _resources[aPath] = resource;
+                    return std::any_cast<Resource &>(_resources[aPath]);
+                } catch (const std::bad_any_cast &e) {
+                    throw ResourcesManagerException("Bad cast: " + std::string(e.what()));
                 }
-                if (_resources[std::type_index(typeid(Resource))].find(aPath)
-                    == _resources[std::type_index(typeid(Resource))].cend()) {
-                    _resources[std::type_index(typeid(Resource))][aPath] = std::any(aLoader(aPath));
-                }
-                return std::any_cast<Resource &>(_resources[std::type_index(typeid(Resource))][aPath]);
             }
 
             /**
-             * @brief Get the Ressource object
+             * @brief Add both a loader and an unloader for a resource
              *
-             * @tparam Resource The type of the resource to get
-             * @param aPath The path of the resource
-             * @return Resource& The resource to get
+             * @tparam Resource The type of the resource
+             * @param aUnloader The unloader
+             * @param aLoader The loader
              */
             template<class Resource>
-            Resource &getRessource(std::string &aPath)
+            void addHandlers(const std::function<void(Resource &)> &aUnloader,
+                             const std::function<Resource &(const std::string &)> &aLoader)
             {
-                if (_resources.find(std::type_index(typeid(Resource))) == _resources.cend()) {
-                    throw std::runtime_error("Ressource not found");
-                }
-                if (_resources[std::type_index(typeid(Resource))].find(aPath)
-                    == _resources[std::type_index(typeid(Resource))].cend()) {
-                    throw std::runtime_error("Ressource not found");
-                }
-                return std::any_cast<Resource &>(_resources[std::type_index(typeid(Resource))][aPath]);
+                addUnloader<Resource>(aUnloader);
+                addLoaders<Resource>(aLoader);
             }
 
         private:
             //-------------- PRIVATE MEMBERS --------------//
-            boost::container::flat_map<std::type_index, boost::container::flat_map<std::string, std::any>> _resources;
+            boost::container::flat_map<std::type_index, std::function<std::any &(const std::string &)>> _loaders;
+            boost::container::flat_map<std::type_index, std::function<void(std::any &)>> _unloader;
+            boost::container::flat_map<std::string, std::any> _resources;
 
         private:
             //-------------- CTOR --------------//
             ResourcesManager() = default;
+
+            /**
+             * @brief Add a loader for a resource
+             *
+             * @tparam Resource The type of the resource
+             * @param aLoader The loader
+             */
+            template<class Resource>
+            void addLoaders(const std::function<Resource &(const std::string &)> &aLoader)
+            {
+                _loaders[std::type_index(typeid(Resource))] = aLoader;
+            }
+
+            /**
+             * @brief Add an unloader for a resource
+             *
+             * @tparam Resource The type of the resource
+             * @param aUnloader The unloader
+             */
+            template<class Resource>
+            void addUnloader(const std::function<void(Resource &)> &aUnloader)
+            {
+                _unloader[std::type_index(typeid(Resource))] = aUnloader;
+            }
+
+            class ResourcesManagerException : public std::exception
+            {
+                public:
+                    explicit ResourcesManagerException(const std::string &aMessage)
+                        : _message(aMessage)
+                    {}
+                    ~ResourcesManagerException() override = default;
+
+                    const char *what() const noexcept override
+                    {
+                        return _message.c_str();
+                    }
+
+                private:
+                    std::string _message;
+            };
     };
 } // namespace ECS::Core
 
