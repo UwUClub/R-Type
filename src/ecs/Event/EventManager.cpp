46c5e5c066cfad479759653d845da7ef6fb3189c
Glassalo <maxence.labourel@epitech.eu>
2023-09-26 09:30:37 +0200

    Updated Event Manager to use pointers

--- src/ecs/Event/EventManager.cpp
+++ src/ecs/Event/EventManager.cpp
@@ -8,9 +8,9 @@ ECS::Event::EventManager::EventManager() = default;
 ECS::Event::EventManager::~EventManager() = default;
 
 //-------------------PUBLIC METHODS-------------------//
-void ECS::Event::EventManager::pushEvent(Event &aEvent)
+void ECS::Event::EventManager::pushEvent(Event *aEvent)
 {
-    _events.push_back(std::move(aEvent));
+    _events.push_back(aEvent);
 }
 
 ECS::Event::EventManager *ECS::Event::EventManager::getInstance()
@@ -20,21 +20,29 @@ ECS::Event::EventManager *ECS::Event::EventManager::getInstance()
     return &instance;
 }
 
-std::vector<ECS::Event::Event> &ECS::Event::EventManager::getEvents()
+std::vector<ECS::Event::Event *> &ECS::Event::EventManager::getEvents()
 {
     return _events;
 }
 
-std::vector<ECS::Event::Event> ECS::Event::EventManager::getEventsByType(const EventType &aEventType)
+std::vector<ECS::Event::Event *> ECS::Event::EventManager::getEventsByType(const EventType &aEventType)
 {
-    std::vector<Event> events;
+    std::vector<Event *> events;
 
     for (auto &event : _events) {
-        if (event.getType() == aEventType) {
+        if (event->getType() == aEventType) {
             events.push_back(event);
         }
     }
     return events;
+}
+
+void ECS::Event::EventManager::clearEvents()
+{
+    for (auto &event : _events) {
+        delete event;
+    }
+    _events.clear();
 }
 
 //-------------------NESTED CLASSES-------------------//
