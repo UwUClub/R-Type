77e1e62c927099fa145616bb428f8e10ab6db8d8
Baptiste Laran <baptiste.laran@epitech.eu>
2023-09-27 11:09:45 +0200

    Update movePlayer system to block player inside the game window

--- src/client/System.cpp
+++ src/client/System.cpp
@@ -3,14 +3,22 @@
 #include "KeyboardEvent.hpp"
 #include "SDLDisplayClass.hpp"
 #include <SDL2/SDL_image.h>
+#include <unordered_map>
+#include <functional>
 
 namespace ECS {
-    void System::movePlayer(Core::World &world, Core::SparseArray<ECS::Utils::Vector2f> &aPos,
-                            Core::SparseArray<ECS::Utils::Speed> &aSpeed,
-                            Core::SparseArray<ECS::Utils::TypeEntity> &aType)
+    void System::movePlayer(Core::World &world, Core::SparseArray<Utils::Vector2f> &aPos,
+                            Core::SparseArray<Utils::Speed> &aSpeed,
+                            Core::SparseArray<Utils::TypeEntity> &aType)
     {
         Event::EventManager *eventManager = Event::EventManager::getInstance();
         auto keyboardEvent = eventManager->getEventsByType(Event::EventType::KEYBOARD);
+        std::unordered_map<const Event::KeyIdentifier, std::function<void(float &, Utils::Vector2f &)>> keyMap = {
+            {Event::KeyIdentifier::UP, [](float &spd, Utils::Vector2f &xy) { xy.y = xy.y <= 0 ? 0 : xy.y -= spd; }},
+            {Event::KeyIdentifier::DOWN, [](float &spd, Utils::Vector2f &xy) { xy.y = xy.y >= SCREEN_HEIGHT ? SCREEN_HEIGHT : xy.y += spd; }},
+            {Event::KeyIdentifier::LEFT, [](float &spd, Utils::Vector2f &xy) { xy.x = xy.x <= 0 ? 0 : xy.x -= spd; }},
+            {Event::KeyIdentifier::RIGHT, [](float &spd, Utils::Vector2f &xy) { xy.x = xy.x >= SCREEN_WIDTH ? SCREEN_WIDTH : xy.x += spd; }},
+        };
 
         for (size_t i = 0; i < aPos.size(); i++) {
             if (!aType[i].has_value() || !aType[i].value().isPlayer) {
@@ -18,14 +26,7 @@ namespace ECS {
             }
             for (auto event : keyboardEvent) {
                 auto keyEvent = static_cast<Event::KeyboardEvent *>(event);
-                if (keyEvent->_keyId == Event::KeyIdentifier::UP)
-                    aPos[i].value().y -= aSpeed[i].value().speed;
-                if (keyEvent->_keyId == Event::KeyIdentifier::DOWN)
-                    aPos[i].value().y += aSpeed[i].value().speed;
-                if (keyEvent->_keyId == Event::KeyIdentifier::LEFT)
-                    aPos[i].value().x -= aSpeed[i].value().speed;
-                if (keyEvent->_keyId == Event::KeyIdentifier::RIGHT)
-                    aPos[i].value().x += aSpeed[i].value().speed;
+                keyMap.at(keyEvent->_keyId)(aSpeed[i].value().speed, aPos[i].value());
             }
         }
     }
