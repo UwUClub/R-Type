a26d19386a3c4ce1acc7ea20dfb3e0f46cf673c2
Baptiste Laran <baptiste.laran@epitech.eu>
2023-10-02 17:50:20 +0200

    Fix background movement

--- src/client/systems/System+MoveBackground.cpp
+++ src/client/systems/System+MoveBackground.cpp
@@ -6,11 +6,13 @@ namespace ECS {
     void System::moveBackground(Core::SparseArray<Utils::Vector2f> &aPos, Core::SparseArray<Component::Speed> &aSpeed,
                                 Core::SparseArray<Component::TypeEntity> &aType)
     {
+        auto deltaTime = Core::World::getInstance().getDeltaTime();
+
         for (size_t i = 0; i < aType.size(); i++) {
             if (!aType[i].has_value() || !aType[i].value().isBackground) {
                 continue;
             }
-            aPos[i].value().x -= aSpeed[i].value().speed * Core::World::getInstance().getDeltaTime();
+            aPos[i].value().x -= aSpeed[i].value().speed * deltaTime;
             if (aPos[i].value().x <= -SCREEN_WIDTH) {
                 aPos[i].value().x = SCREEN_WIDTH;
             }
