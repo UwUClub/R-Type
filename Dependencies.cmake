ad727720285f5f05e93d14a51f403054304b07c2
Glassalo <maxence.labourel@epitech.eu>
2023-10-14 18:38:22 +0200

    Checkout submodules

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -1,6 +1,5 @@
 include(cmake/CPM.cmake)
 include(FetchContent)
-include(cmake/FindSDL2.cmake)
 
 # Done as a function so that updates to variables like
 # CMAKE_CXX_FLAGS don't propagate out to other
@@ -9,8 +8,6 @@ function(R_Type_setup_dependencies)
 
   #find_package(Catch2 QUIET)
   find_package(Boost QUIET COMPONENTS system serialization align assert config core static_assert throw_exception array bind chrono integer move mpl predef asio ratio type_traits typeof utility coroutine date_time function regex smart_ptr preprocessor io uuid QUIET)
-  find_package(SDL2 QUIET)
-  find_package(SDL2_image QUIET)
 
   #if(NOT TARGET Catch2::Catch2WithMain)
    # CPMAddPackage(
@@ -690,40 +687,6 @@ function(R_Type_setup_dependencies)
             GIT_TAG "boost-1.83.0"
             GIT_SHALLOW TRUE
     )
-  endif()
-
-
-  if (NOT TARGET SDL2)
-    FetchContent_Declare(
-            SDL2
-            GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
-            GIT_TAG release-2.28.3
-            GIT_SHALLOW TRUE
-            GIT_PROGRESS TRUE
-            CMAKE_ARGS -DBUILD_SHARED_LIBS=OFF -DSDL_STATIC=ON
-    )
-  endif()
-  FetchContent_MakeAvailable(SDL2)
-  install(TARGETS SDL2-static
-          EXPORT SDL2Targets
-          RUNTIME DESTINATION bin
-          LIBRARY DESTINATION lib
-          ARCHIVE DESTINATION lib
-          INCLUDES DESTINATION include
-          PUBLIC_HEADER DESTINATION include
-          )
-
-  if (NOT TARGET SDL2_image::SDL2_image)
-    FetchContent_Declare(
-            SDL2_image
-            GIT_REPOSITORY https://github.com/libsdl-org/SDL_image.git
-            GIT_TAG release-2.6.3
-            GIT_SHALLOW TRUE
-            GIT_PROGRESS TRUE
-            CMAKE_ARGS -DBUILD_SHARED_LIBS=OFF -DSDL_STATIC=ON
-    )
-    set(SDL2IMAGE_INSTALL OFF)
-    FetchContent_MakeAvailable(SDL2_image)
   endif()
 
 endfunction()
