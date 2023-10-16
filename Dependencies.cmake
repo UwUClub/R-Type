51e609a3bf76c35f47b5a1f55876d429501637a2
Glassalo <maxence.labourel@epitech.eu>
2023-10-16 13:54:00 +0200

    Fixed dependencies

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -10,17 +10,6 @@ function(R_Type_setup_dependencies)
   find_package(Boost QUIET COMPONENTS system serialization align assert config core static_assert throw_exception array bind chrono integer move mpl predef asio ratio type_traits typeof utility coroutine date_time function regex smart_ptr preprocessor io uuid QUIET)
   find_package(raylib)
 
-<<<<<<< HEAD
-#   if(NOT TARGET Catch2::Catch2WithMain)
-#     CPMAddPackage(
-#             NAME Catch2
-#             VERSION 3.4.0
-#             GITHUB_REPOSITORY catchorg/Catch2
-#             GIT_TAG v3.4.0
-#             GIT_SHALLOW TRUE
-#     )
-#   endif()
-=======
   #if(NOT TARGET Catch2::Catch2WithMain)
    # CPMAddPackage(
     #        NAME Catch2
@@ -30,7 +19,6 @@ function(R_Type_setup_dependencies)
         #    GIT_SHALLOW TRUE
     #)
   #endif()
->>>>>>> 7e7098225af0fd2bf4dff6298bf1d8de9b706d5d
 
   if (NOT TARGET Boost::system)
     CPMAddPackage(
