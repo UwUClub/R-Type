507d5643fef2f9e0ed75dae5d7f998d4a873f8cd
Valegox <geg.valentin@gmail.com>
2023-10-18 01:01:31 +0200

    Save debugging progress

--- src/rtype/NetworkHandler.hpp
+++ src/rtype/NetworkHandler.hpp
@@ -23,7 +23,11 @@ namespace Network {
         private:
             boost::asio::io_service _ioService;
             udp::socket _socket = udp::socket(_ioService);
-            std::array<char, READ_BUFFER_SIZE> _readBuffer = std::array<char, READ_BUFFER_SIZE>();
+
+            //std::array<char, READ_BUFFER_SIZE> _readBuffer = std::array<char, READ_BUFFER_SIZE>();*
+            boost::asio::streambuf _readInbound;
+            boost::asio::streambuf::mutable_buffers_type _readBuffer = _readInbound.prepare(READ_BUFFER_SIZE);
+
             udp::endpoint _readEndpoint;
             std::function<void(const RType::Packet &, udp::endpoint &)> _onReceive;
             std::function<void(const std::string, udp::endpoint &)> _onReceiveAknowledgment;
