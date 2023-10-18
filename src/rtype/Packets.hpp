4ee0887a144dae0519cade313fc8fcf54292319c
Clang Robot <robot@uwuclub.com>
2023-10-18 08:08:13 +0000

    :art: Committing clang-format changes

--- src/rtype/Packets.hpp
+++ src/rtype/Packets.hpp
@@ -47,9 +47,9 @@ namespace RType {
             template<typename archive>
             void serialize(archive &ar, const unsigned int /*version*/)
             {
-                ar & uuid;
-                ar & type;
-                ar & payload;
+                ar &uuid;
+                ar &type;
+                ar &payload;
             }
     };
 
