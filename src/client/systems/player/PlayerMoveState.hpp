e3550ef84593d0be8c8ad8e9aad9ee1188d4e305
Clang Robot <robot@uwuclub.com>
2023-10-30 10:14:20 +0000

    :art: Committing clang-format changes

--- src/client/systems/player/PlayerMoveState.hpp
+++ src/client/systems/player/PlayerMoveState.hpp
@@ -1,20 +1,21 @@
 #ifndef PLAYERMOVESTATE_HPP_
 #define PLAYERMOVESTATE_HPP_
 
-struct PlayerMoveState {
-    PlayerMoveState() = default;
-    ~PlayerMoveState() = default;
+struct PlayerMoveState
+{
+        PlayerMoveState() = default;
+        ~PlayerMoveState() = default;
 
-    static PlayerMoveState &getInstance()
-    {
-        static PlayerMoveState instance;
-        return instance;
-    }
+        static PlayerMoveState &getInstance()
+        {
+            static PlayerMoveState instance;
+            return instance;
+        }
 
-    bool runningX = false;
-    bool runningY = false;
-    float speedX = 0;
-    float speedY = 0;
+        bool runningX = false;
+        bool runningY = false;
+        float speedX = 0;
+        float speedY = 0;
 };
 
 #endif /* !PLAYERMOVESTATE_HPP_ */
